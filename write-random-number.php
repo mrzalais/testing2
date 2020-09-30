9c5d0155e1d3465ba5ac83ebf4052b56a245b32b
mrzalais <64210341+mrzalais@users.noreply.github.com>
2020-09-30 10:35:23 +0300

    Fixed spacing

--- write-random-number.php
+++ write-random-number.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-function giveRandom():int
+function giveRandom(): int
 {
-    return rand(1,1000);
+    return rand(1, 1000);
 }
 
 echo "Random number:";
@@ -12,11 +12,11 @@ echo $random = giveRandom();
 echo PHP_EOL;
 
 $filename = file('numbers.txt');
-file_put_contents("numbers.txt",$random,FILE_APPEND);
-file_put_contents("numbers.txt"," ",FILE_APPEND);
+file_put_contents("numbers.txt", $random, FILE_APPEND);
+file_put_contents("numbers.txt", " ", FILE_APPEND);
 
 $number = explode(' ', file_get_contents('numbers.txt'));
 $count = count($number) - 1;
-$average = array_sum($number)/$count;
+$average = array_sum($number) / $count;
 echo round($average, 2);
 echo PHP_EOL;
